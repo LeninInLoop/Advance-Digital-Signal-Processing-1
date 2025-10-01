d5d266777abc058cac11121fa325fb5356b24214
Adib <adib.n7789@gmail.com>
2025-10-01 19:27:03 +0330

    Fix Some Bugs and add MSE

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import numpy as np
 class Helper:
     @staticmethod
     def generate_random_matrix(shape: Tuple[int, int]) -> np.ndarray:
-        return np.random.rand(*shape)
+        return np.random.rand(*shape) * 100
+
+    @staticmethod
+    def calculate_mse(a: np.ndarray, b: np.ndarray) -> float:
+        return np.mean((a - b) ** 2)
 
     @staticmethod
     def print_matrix(matrix: np.ndarray, precision: int = 4):
@@ -32,14 +36,37 @@ class SVDCalculator:
         return np.diag(singular_values_sorted), phi_sorted
 
     @staticmethod
-    def calculate_si_from_phi(matrix: np.ndarray, singular_values: np.ndarray, phi: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    def calculate_si_from_phi(
+            matrix: np.ndarray,
+            singular_values: np.ndarray,
+            phi: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
         inverse_singular_values = np.linalg.pinv(singular_values)
         si_sorted = matrix @ phi @ inverse_singular_values
         return si_sorted
 
     @staticmethod
-    def calculate_original_matrix(singular_value: np.ndarray, phi: np.ndarray, si: np.ndarray) -> np.ndarray:
+    def calculate_original_matrix(
+            singular_value: np.ndarray,
+            phi: np.ndarray,
+            si: np.ndarray
+    ) -> np.ndarray:
         return si @ singular_value @ phi.T
+
+    @staticmethod
+    def reconstruct_approximated_matrix(
+            si: np.ndarray,
+            singular_value_matrix: np.ndarray,
+            phi: np.ndarray,
+            k: int
+    ) -> np.ndarray:
+        if k > singular_value_matrix.shape[0]:
+            raise IndexError(f"k must be smaller than {singular_value_matrix.shape[0]}")
+
+        si_k = si[:, :k]  # First k columns of U
+        singular_value_k = singular_value_matrix[:k, :k]  # Top-left kxk block of Sigma
+        phi_k = phi[:, :k]  # First k columns of V
+        return si_k @ singular_value_k @ phi_k.T
 
 def main():
     random_matrix = Helper.generate_random_matrix(shape = (16, 16))
@@ -67,7 +94,21 @@ def main():
     print("\n\n", 50 * "=" , "\nRecalculated Matrix :")
     Helper.print_matrix(recalculated_matrix)
 
-    print(f"\nAre the matrices close? {np.allclose(random_matrix, recalculated_matrix)}")
+    print("\n\n", 50 * "=", f"\nMSE between Original and Reconstructed Matrix:")
+    print(Helper.calculate_mse(random_matrix, recalculated_matrix))
+
+    k = 14
+    approximated_matrix = SVDCalculator.reconstruct_approximated_matrix(
+        si,
+        singular_value_from_phi,
+        phi,
+        k
+    )
+    print("\n\n", 50 * "=" , f"\nApproximation Matrix Using only {k} singular values:")
+    Helper.print_matrix(approximated_matrix)
+
+    print("\n\n", 50 * "=" , f"\nMSE of Approximation Matrix Using only {k} singular values:")
+    print(Helper.calculate_mse(random_matrix, approximated_matrix))
 
 if __name__ == '__main__':
     main()
